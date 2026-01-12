a64250692bf6d79ee59687eeb3a148465e08cd12
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2026-01-13 00:00:06 +0800

    Implements social authentication flow
    Adds Google social authentication using Supabase.
    Includes callback route to handle authentication redirects.
    Adds SocialAuthButtons component for initiating the OAuth flow.
    Updates proxy to allow callback to be unproxied.
    Updates Supabase proxy to allow callback to be unproxied.
    Removes default landing page and favicon.

--- components/socialauth-buttons.tsx
+++ components/socialauth-buttons.tsx
@@ -1,11 +1,41 @@
+"use client"
+
 import { Button } from '@/components/ui/button'
 import { FcGoogle as GoogleIcon } from 'react-icons/fc'
 import { FaGithub as GithubIcon } from 'react-icons/fa'
+import { createClient } from '@/utils/supabase/client'
+import { toast } from 'sonner'
+import { useState } from 'react'
 
 const SocialAuthButtons = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signInWithGoogle = async () => {
+    const supabase = createClient();
+
+    const URL = `${window.location.origin}/api/auth/callback`
+
+    setIsLoading(true);
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: URL,
+      }
+    })
+
+    if (error) {
+      toast.error('Error during Google OAuth sign-in');
+    } else {
+      toast.success('Redirecting to Google for authentication...');
+    }
+
+    setIsLoading(false);
+  }
+
   return (
     <>
-      <Button variant="outline" className="w-full gap-2">
+      <Button variant="outline" className="w-full gap-2" onClick={signInWithGoogle}>
           <GoogleIcon /> Continue with Google
       </Button>
       <Button variant="outline" className="w-full gap-2 mt-2">
--- utils/supabase/proxy.ts
+++ utils/supabase/proxy.ts
@@ -34,7 +34,8 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/register')
+    !request.nextUrl.pathname.startsWith('/register') &&
+    !request.nextUrl.pathname.startsWith('/api/auth/callback')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
